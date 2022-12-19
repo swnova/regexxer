2268228b519717c8464f9be26db7a1ec16173310
swnova <sharrodwillanova@gmail.com>
2022-12-19 01:10:52 -0800

    code for user and thought models

--- models/user.js
+++ models/user.js
@@ -17,8 +17,31 @@ const userSchema = new Schema(
         },
         thoughts: [
             {
-                type: Schema
+                type: Schema.Types.ObjectId,
+                ref: "Thought"
             }
-        ]
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
     }
-) 
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+
+module.exports = User;
